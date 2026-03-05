aaa4eeac9acf2863d3e2ad91ab479166389e19fe
kishanth12 <kishanth1209@gmail.com>
2026-03-05 11:39:39 +0530

    feat: add user register and login functionality

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,59 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import User from "./../models/user.model.js";
+
+const generateToken = (userId) =>
+  jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+  });
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const newUser = await user.save();
+    const token = generateToken(user._id);
+    res.status(201).json({
+      message: "User registered successfully",
+      user: newUser,
+      token,
+    });
+  } catch (error) {
+    console.log("Error registering user:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+//login
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const token = generateToken(user._id);
+    res.status(200).json({
+      message: "User logged in successfully",
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log("Error logging in user:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
