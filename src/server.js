1e512ba04bf46b5c4cee16782e8d1b60356b6b7a
kishanth12 <kishanth1209@gmail.com>
2026-03-05 11:45:36 +0530

    feat: integrate routes and Swagger into server setup

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
+import ticketRoutes from "./routes/ticket.routes.js";
+import { swaggerDocs } from "./config/swagger.js";
 
 dotenv.config();
 connectDb();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tickets", ticketRoutes);
+swaggerDocs(app, port);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
